2c8d72dd50800c625fd4e3fd7ab2ac286ee98105
Jose Rilla <jose@rilla.es>
2026-08-05 10:49:33 +0000

    Pass bare GOBL Net addresses as signing claims

--- internal/ops/sign.go
+++ internal/ops/sign.go
@@ -4,9 +4,9 @@ import (
 	"context"
 
 	"github.com/invopop/gobl"
-	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/dsig"
 	"github.com/invopop/gobl/head"
+	"github.com/invopop/gobl/net"
 )
 
 // SignOptions are the options used for signing a GOBL document.
@@ -14,11 +14,11 @@ type SignOptions struct {
 	*ParseOptions
 	PrivateKey *dsig.PrivateKey
 
-	// Issuer is the signer's verifiable GOBL Net address (a gobl: URI) and
-	// Audience is the optional GOBL Net audience the signature is bound to;
-	// either may be empty.
-	Issuer   cbc.URI
-	Audience cbc.URI
+	// Issuer is the signer's verifiable GOBL Net address and Audience is the
+	// optional GOBL Net audience the signature is bound to; either may be
+	// empty.
+	Issuer   net.Address
+	Audience net.Address
 }
 
 // Sign parses a GOBL document into an envelope, performs calculations,
@@ -45,10 +45,10 @@ func Sign(ctx context.Context, opts *SignOptions) (*gobl.Envelope, error) {
 	// Sign envelope headers. Validation is done transparently in `Sign`.
 	var signOpts []head.SignOption
 	if opts.Issuer != "" {
-		signOpts = append(signOpts, head.WithIssuer(opts.Issuer))
+		signOpts = append(signOpts, head.WithIssuer(opts.Issuer.String()))
 	}
 	if opts.Audience != "" {
-		signOpts = append(signOpts, head.WithAudience(opts.Audience))
+		signOpts = append(signOpts, head.WithAudience(opts.Audience.String()))
 	}
 	if err := env.Sign(opts.PrivateKey, signOpts...); err != nil {
 		return nil, gobl.ErrInternal.WithCause(err)
